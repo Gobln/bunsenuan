038abd08d95d70a9f2b2f2823ec1c16d71703c03
Gobln <bluewidow@gmail.com>
2022-06-28 17:20:00 +0100

    test

---
+++ initlabs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp sources.list /etc/apt/sources.list
+wget https://pkg.bunsenlabs.org/debian/pool/main/b/bunsen-keyring/bunsen-keyring_2020.10.10%2Bbl11-2_all.deb
+dpkg -i bunsen-keyring_2020.10.10%2Bbl11-2_all.deb
+apt-get update
+apt -y install openbox pulseaudio xserver-xorg-video-ati xserver-xorg-video-radeon xserver-xorg-video-nvidia xserver-xorg-video-nouveau bunsen-meta-all
+apt -y purge xfce4-notifyd
+chmod 7777 /sbin/shutdown
+cp bl-exit /usr/bin/bl-exit
+echo "Done. Rebooting..."
+shutdown -r now
